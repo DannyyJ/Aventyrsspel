c75c9724d64cc8dcefa59adaa6880ab8bcc00ae5
CharlesSoder <charles.soderstrom@skola.taby.se>
2023-11-13 08:55:40 +0100

    la till en mainloop blabla

--- spel.py
+++ spel.py
@@ -1,2 +1,36 @@
-print("hej")
-print("aaa")
+import random as rand
+
+def main():
+    hjältenamn = "Lennart"
+    hjältehp = 10
+    hjältestyrka = rand.randint(0,10)
+
+    while hjältehp >= 0:        
+        print(
+            """
+
+            Vad vill du göra?
+            1. Välj dörr
+            2. Kolla ryggsäck
+            3. Kolla stats
+            
+            """)
+        
+        val = input("")
+
+        if val == "1":
+            pass
+        elif val == "2":
+            pass
+        elif val == "3":
+            pass
+        else:
+            print("Välj 1, 2 eller 3!")
+
+main()
+
+class zombie():
+    def __init__()
+    Health = 
+    Strenght = 
+    Färg = 
