e1b0718eca277d77886034e7d82cec909cb3e799
CharlesSoder <charles.soderstrom@skola.taby.se>
2023-11-14 11:08:01 +0100

    Gjorde klart dörr system

--- Charles-Äventyrspel.py
+++ Charles-Äventyrspel.py
@@ -6,6 +6,12 @@ class Zombie():
         self.styrka = styrka
         self.färg = färg
 
+def Fälla():
+
+def Monster():
+
+def Kista():
+
 def main():
     hjältenamn = "Lennart"
     hjältehp = 10
@@ -35,16 +41,38 @@ def main():
         val = input("")
         if val == "1":
 
+            def dörr1():
+                Händelser = ["Fälla", "Monster", "Kista"]
+                Vald_Händelse = rand.choice(Händelser)
+                print(f"Bakom dörr 1: {Vald_Händelse}")
+
+            def dörr2():
+                Händelser = ["Fälla", "Monster", "Kista"]
+                Vald_Händelse = rand.choice(Händelser)
+                print(f"Bakom dörr 2: {Vald_Händelse}")
+
+            def dörr3():
+                Händelser = ["Fälla", "Monster", "Kista"]
+                Vald_Händelse = rand.choice(Händelser)
+                print(f"Bakom dörr 3: {Vald_Händelse}")
+
+                if Vald_Händelse == "Fälla":
+                    # Kalla på "def fälla"
+                elif Vald_Händelse == "Monster":
+                    # Kalla på "def monster"
+                elif Vald_Händelse == "Kista":
+                    # Kalla på "def kista"
+                
             dörrval = " "
             while dörrval not in "123":
                 print("Du ser 3 olika dörrar. Välj dörr 1, 2 eller 3")
                 dörrval = input("")
                 if dörrval == "1":
-                    pass
+                    dörr1()
                 elif dörrval == "2":
-                    pass
+                    dörr2()
                 elif dörrval == "3":
-                    pass
+                    dörr3()
                 else:
                     print(f"dörr {dörrval} finns inte. Välj dörr 1, 2 eller 3!")
 
